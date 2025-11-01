6d84ee762d02f739a4091d50c4aa44fab51c88cc
Filipe Rosa <filiperosa0312@gmail.com>
2025-11-01 14:04:00 -0300

    Created StripeService

---
+++ BackEnd/MarketplaceArtesanato.Services/Services/StripeService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Stripe;
+using Stripe.Checkout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketplaceArtesanato.Services.Services
+{
+    public class StripeService
+    {
+        private readonly string _secretKey;
+        private readonly string _webhookSecret;
+
+        public StripeService (IConfiguration configuration)
+        {
+            _secretKey = configuration["Stripe:SecretKey"];
+            _webhookSecret = configuration["Stripe:WebhookSecret"];
+            StripeConfiguration.ApiKey = _secretKey;
+        }
+    }
+}
