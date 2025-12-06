b38c1dae31d460fbe925247f83e50f226aea5da0
Filipe Rosa <filiperosa0312@gmail.com>
2025-12-06 11:32:23 -0300

    Hotfiz

--- FrontEnd/frontend-marktplace/src/app/app.routes.server.ts
+++ FrontEnd/frontend-marktplace/src/app/app.routes.server.ts
@@ -1,49 +1,59 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Rotas dinâmicas: Client Side (Navegador busca dados)
   {
     path: 'products/:id',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Client
   },
   {
     path: 'sellers/:id',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Client
   },
   {
-    path: 'profile',
-    renderMode: RenderMode.Server
+    path: 'categorias/:slug',
+    renderMode: RenderMode.Client
   },
+  {
+    path: 'categorias',
+    renderMode: RenderMode.Client
+  },
+
   {
     path: 'checkout',
     renderMode: RenderMode.Client
   },
   {
-    path: 'forgot-password',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'reset-password',
-    renderMode: RenderMode.Server
-  },
-  {
     path: 'cart',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Client
   },
   {
     path: 'orders',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'profile',
+    renderMode: RenderMode.Client
   },
   {
     path: 'seller-dashboard',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Client
   },
   {
     path: 'add-product',
     renderMode: RenderMode.Client
   },
+  {
+    path: 'favorites',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'admin',
+    renderMode: RenderMode.Client
+  },
 
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
